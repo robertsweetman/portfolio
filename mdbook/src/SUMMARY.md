776b02e0959c14d709eaca508e01086f6f8dd568
Robert Sweetman <robertsweetman@pm.me>
2025-12-31 11:35:14 +0000

    add mdbook initial files

---
+++ mdbook/src/SUMMARY.md
@@ -0,0 +1,3 @@
+# Summary
+
+- [Section](./section.md)
---
+++ mermaid-init.js
@@ -0,0 +1,12 @@
+mermaid.initialize({
+    startOnLoad: true,
+    theme: 'dark',
+    themeVariables: {
+      primaryColor: '#2b79a2',
+      primaryTextColor: '#c8c9db',
+      primaryBorderColor: '#3b3f51',
+      lineColor: '#c8c9db',
+      secondaryColor: '#272935',
+      tertiaryColor: '#161923'
+    }
+  });
